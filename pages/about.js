e8d2eb33158b41f7cb565ff6609a1149a65844b0
Sylvie Fiquet <sfiquetdev@gmail.com>
2020-11-04 11:03:31 +0000

    feat: add about page

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Layout from '../components/layout'
+
+function About(){
+  return (
+    <Layout>
+      <div className="space-y-4">
+        <p>
+          This site is a prototype for a <a href="http://worldoftwilight.com">World of Twilight</a> reference. Currently the purpose is to gather feedback on what players need.
+        </p>
+        <p>
+          Please get in touch and tell me what works, what doesn't, and which new features would make it really special.
+        </p>
+      </div>
+    </Layout>
+  );
+}
+
+export default About;
+
