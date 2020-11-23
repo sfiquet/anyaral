a6678af1e3770cbcb45165c4f1266cdca1e7571c
Sylvie Fiquet <sfiquetdev@gmail.com>
2020-11-23 18:46:55 +0000

    improve home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { appName } from '../app.config.js'
 import Meta from '../components/meta'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import styles from '../styles/Home.module.css'
 
 function HomeLayout({ children }){
   return (
@@ -20,32 +19,50 @@ function HomeLayout({ children }){
   )
 }
 
+function FeatureList({ children }){
+  // temporary one-column grid while there's only one option
+  // TO DO: add sm:grid-cols-2 when another item is added
+  
+  return (
+    <ul className="grid grid-cols-1 gap-4 w-full sm:w-auto max-w-3xl">
+      { children }
+    </ul>
+  )
+}
+
+function FeatureListItem({ title, description, url, uid }){
+  let descId = `${uid}-desc`
+  return (
+    <li id={ uid } className="text-center p-6 border border-solid border-gray-300 rounded-xl transition-colors duration-150 ease-in-out hover:border-blue-400">
+      <a href={ url } aria-describedby={ descId }>
+        <h3 className="mb-4 text-2xl" aria-label={ title }>{`${title} →`}</h3>
+      </a>
+      <p id={ descId } className="text-xl">{ description }</p>
+    </li>
+  )
+}
+
 export default function Home() {
   return (
     <HomeLayout>
 
       <main className="flex flex-col justify-evenly items-center space-y-12">
         <div className="space-y-4">
-          <h1 className="text-center tracking-tight">
+          <h1 className="text-center tracking-tight text-4xl">
             Welcome
           </h1>
 
-          <p className="text-lg text-center">
+          <p className="text-xl text-center">
             {appName} is a reference app for players of <a href="http://worldoftwilight.com">World of Twilight</a>.
           </p>
         </div>
 
         <div className="space-y-4">
-          <h2 className="text-center text-xl">Features currently available:</h2>
-
-          {/*<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full sm:w-auto max-w-3xl">*/}
-          {/* temporary one-column grid while there's only one option */}
-          <div className="grid grid-cols-1 gap-4 w-full sm:w-auto max-w-3xl">
-            <a href="/denizens/" className={styles.card}>
-              <h3>Denizens <span aria-hidden="true">&rarr;</span></h3>
-              <p>Browse denizens of Anyaral by race</p>
-            </a>
-          </div>
+          <h2 className="text-center text-3xl">Features</h2>
+          <FeatureList>
+            <FeatureListItem title="Denizens" description="Browse denizens of Anyaral by race" 
+              url="/denizens/" uid="feat-denizens" />
+          </FeatureList>
         </div>
 
       </main>
