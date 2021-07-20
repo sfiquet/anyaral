bc0eeed64db970388e7712f77cedf3eb6dbd1e79
Sylvie Fiquet <sfiquetdev@gmail.com>
2021-07-20 15:15:45 +0100

    WIP generate BuildId to use as revision number for HTML files
    - only implemented for denizen pages
    - To DO: all the other pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require('next-pwa')
+const nextBuildId = require('next-build-id')
 
 // set up properties used in next-pwa code to precache the public folder
 const subdomainPrefix = ''
@@ -15,7 +16,8 @@ const getRevision = data => crypto.createHash('md5').update(data).digest('hex')
 const getFileRevision = file => getRevision(fs.readFileSync(file))
 
 // precache files in public folder
-let manifestEntries = globby
+function getStaticPrecacheEntries(){
+  let manifestEntries = globby
   .sync(
     [
       '**/*',
@@ -37,35 +39,49 @@ let manifestEntries = globby
     url: path.posix.join(subdomainPrefix, `/${f}`),
     revision: getFileRevision(`public/${f}`)
   }))
+  return manifestEntries
+}
+
 // ** end of next-pwa code
 
-// extract the list of all denizen pages
 
-// functions copied from lib/api.js since you can't import it
+// utility functions copied from lib/api.js since you can't import it
 const dataDir = path.posix.join(process.cwd(), 'data');
+
 function readJsonFile(dataFile){
   let json = fs.readFileSync(dataFile, {encoding: 'utf8'});
   return JSON.parse(json);
 }
+
 function getAllCreatures(){
   let rawObj = readJsonFile(path.posix.join(dataDir, 'creatures_list.json'));
   // version checking would take place here if needed
   return rawObj.data;
 }
 
-let denizenPath = '/denizens/'
-let denizens = getAllCreatures()
-let htmlEntries = denizens.map(denizen => ({
-  url: path.posix.join(denizenPath, denizen.code),
-  revision: getRevision(JSON.stringify(denizen)),
-}))
-//console.log('htmlEntries', htmlEntries)
-//console.log('manifestEntries', manifestEntries)
+// extract the list of all denizen pages
+function getDenizenPages(){
+  let denizenPath = '/denizens/'
+  let buildId = nextBuildId.sync()
+  let denizens = getAllCreatures()
+  let htmlEntries = denizens.map(denizen => ({
+    url: path.posix.join(denizenPath, denizen.code),
+    revision: buildId,
+  }))
+  //console.log('htmlEntries', htmlEntries)
+  return htmlEntries
+}
+
+function getGeneratedPrecacheEntries(){
+  // TO DO: add other pages
+  return getDenizenPages()
+}
 
 module.exports = withPWA({
+  generateBuildId: () => nextBuildId(),
   pwa: {
     dest: 'public',
-    additionalManifestEntries: [...manifestEntries, ...htmlEntries],
+    additionalManifestEntries: [...getStaticPrecacheEntries(), ...getGeneratedPrecacheEntries()],
     subdomainPrefix,
     publicExcludes,
     sw,
