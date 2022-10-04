3a012f64632a2202a1bca034e642cb538e8021df
Sylvie Fiquet <sfiquetdev@gmail.com>
2022-10-04 09:00:05 +0100

    chore: remove useless pwa optimisations

--- next.config.js
+++ next.config.js
@@ -1,40 +1,34 @@
-let buildId
+const { nanoid } = require('nanoid')
+const getStaticPrecacheEntries = require('./util/staticprecache.js')
+const getGeneratedPrecacheEntries = require('./util/precache.js')
+
+const buildId = nanoid()
 
 const nextConfig = {
   generateBuildId: () => buildId,
 }
 
-let pwa = {
+const cacheEntries = [
+  ...getStaticPrecacheEntries({
+    // exclude icon-related files from the precache since they are platform specific
+    // note: no need to pass publicExcludes to next-pwa, it's not used for anything else
+    publicExcludes: [
+      '!*.png',
+      '!*.ico',
+      '!browserconfig.xml',
+    ],
+  }), 
+  ...getGeneratedPrecacheEntries(buildId),
+]
+
+const pwa = {
+  additionalManifestEntries: cacheEntries,
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   dynamicStartUrl: false, // precache home page instead of storing it in runtime cache by default
   register: false,
   skipWaiting: false,
   buildExcludes: [/middleware-manifest\.json$/], // for Next 12, see https://github.com/shadowwalker/next-pwa/issues/288
-}
-
-// Attribute additionalManifestEntries is only needed for the build and 
-// time-consuming to generate.
-// So we only calculate it if we are actually generating the production build
-if (process.env.PRECACHE === 'true'){
-  const getStaticPrecacheEntries = require('./util/staticprecache.js')
-  const getGeneratedPrecacheEntries = require('./util/precache.js')
-  const { nanoid } = require('nanoid')
-
-  buildId = nanoid()
-
-  pwa.additionalManifestEntries = [
-    ...getStaticPrecacheEntries({
-      // exclude icon-related files from the precache since they are platform specific
-      // note: no need to pass publicExcludes to next-pwa, it's not used for anything else
-      publicExcludes: [
-        '!*.png',
-        '!*.ico',
-        '!browserconfig.xml',
-      ],
-    }), 
-    ...getGeneratedPrecacheEntries(buildId),
-  ]
 }
 
 const withPWA = require('next-pwa')(pwa)
